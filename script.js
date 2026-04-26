3deda36ab155a064166719564491b6c11460a309
Lucas Abel <lucasabel783@gmail.com>
2026-04-26 14:14:49 +0100

    Add mobile menu toggle functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+// MENU MOBILE
+const toggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+toggle.onclick = () => {
+  nav.classList.toggle('active');
+};
